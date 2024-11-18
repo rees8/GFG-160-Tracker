f42b45e61df824dad24e53c50973b2d832435152
Rhea <88110289+rees8@users.noreply.github.com>
2024-11-18 12:53:24 +0530

    Create ReverseArray.java

---
+++ 1. Arrays/3. Reverse an Array/ReverseArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        int n = arr.length;
+        for(int i=0; i<n/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
+        }
+    }
+}
+
+// TC = O(n)
+// SC = O(1)
